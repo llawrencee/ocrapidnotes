8303e4d1b3b2d4f2ff06af2fef365569b866bb17
Lawrence <143608054+llawrencee@users.noreply.github.com>
2024-02-18 18:21:15 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// Libraries
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// Styling
+import "./css/index.css"
+
+// Components
+import App from "./App.jsx"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
